52b73b2ef5b35101406657b0751d5a2e11f7cf13
Krzysztof Wojciechowski <49921081+Kotochleb@users.noreply.github.com>
2024-09-30 15:39:05 +0200

    Add simulation support for RGB and stereo cameras (#78)

--- rae_gazebo/launch/gazebo.launch.py
+++ rae_gazebo/launch/gazebo.launch.py
@@ -8,15 +8,16 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, Opaq
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 
 def launch_setup(context, *args, **kwargs):
     rae_description_pkg = get_package_share_directory('rae_description')
     ros_gz_sim_pkg = get_package_share_directory('ros_gz_sim')
-    enable_localization = LaunchConfiguration(
-        'enable_localization', default='true')
+    namespace = LaunchConfiguration('namespace')
+    enable_localization = LaunchConfiguration('enable_localization')
     world_file = LaunchConfiguration('sdf_file').perform(context)
     print(f'world_file: {world_file}')
+
     return [
         SetEnvironmentVariable(
             name='IGN_GAZEBO_RESOURCE_PATH',
@@ -42,15 +43,18 @@ def launch_setup(context, *args, **kwargs):
                 os.path.join(rae_description_pkg, 'launch', 'rsp.launch.py')
             ]),
             launch_arguments={
-                'use_sim_time': 'True'
+                'use_sim_time': 'True',
+                'namespace': namespace
             }.items()
         ),
 
         # ros_gz_bridge
         Node(
             package='ros_gz_bridge',
+            namespace=LaunchConfiguration('namespace'),
             executable='parameter_bridge',
             parameters=[{
+                'expand_gz_topic_names': True,
                 'use_sim_time': True,
                 'config_file': os.path.join(get_package_share_directory(
                     'rae_gazebo'),'config', 'gz_bridge.yaml')
@@ -61,8 +65,11 @@ def launch_setup(context, *args, **kwargs):
         # Ignition Gazebo - Spawn Entity
         Node(
             package='ros_gz_sim',
+            namespace=LaunchConfiguration('namespace'),
             executable='create',
             arguments=[
+                "-name", "rea",
+                "-allow_renaming", "true",
                 '-topic', 'robot_description',
             ],
             output='screen'
@@ -73,7 +80,8 @@ def launch_setup(context, *args, **kwargs):
             package='controller_manager',
             namespace=LaunchConfiguration('namespace'),
             executable='spawner',
-            arguments=['diff_controller', '-c', '/controller_manager'],
+            arguments=['diff_controller',
+                       '--controller-manager', f'/controller_manager'],
         ),
 
         # Activate joint state broadcaster
@@ -82,7 +90,7 @@ def launch_setup(context, *args, **kwargs):
             executable='spawner',
             namespace=LaunchConfiguration('namespace'),
             arguments=['joint_state_broadcaster',
-                    '--controller-manager', '/controller_manager'],
+                       '--controller-manager', f'/controller_manager'],
         ),
 
         # Activate EKF
@@ -93,9 +101,13 @@ def launch_setup(context, *args, **kwargs):
             name='ekf_filter_node',
             namespace=LaunchConfiguration('namespace'),
             output='screen',
-            parameters=[os.path.join(get_package_share_directory(
-                    'rae_hw'), 'config', 'ekf.yaml')],
-        )
+            parameters=[{'use_sim_time': True},
+                        os.path.join(get_package_share_directory(
+                        'rae_hw'), 'config', 'ekf.yaml')],
+        ),
+
+        # Set all nodes to use simulation time
+        SetParameter(name='use_sim_time', value=True)
     ]
 
 
@@ -103,6 +115,8 @@ def launch_setup(context, *args, **kwargs):
 def generate_launch_description():
     rae_gazebo_pkg = get_package_share_directory('rae_gazebo')
     declared_arguments = [
+        DeclareLaunchArgument('namespace', default_value=''),
+        DeclareLaunchArgument('enable_localization', default_value='true'),
         DeclareLaunchArgument('sdf_file', default_value=f"-r {os.path.join(rae_gazebo_pkg, 'worlds', 'world_demo.sdf')}"),
     ]
 
